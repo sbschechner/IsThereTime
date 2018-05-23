7110879f26275d641b49b52ce20ce26c852fbf7d
benjy <benjamin.schechner@gmail.com>
2018-05-23 12:47:43 -0400

    fixed timezones

--- src/sunset.js
+++ src/sunset.js
@@ -6,8 +6,8 @@ class Sunset extends Component {
   constructor(props){
     super(props);
     this.state = {
-        sunsetTime: "12:15:26 AM", //THIS SHOULD BE NULL WHEN BACK.
-        hasSunset: true, //this should be false when back
+        sunsetTime: null,
+        hasSunset: false, 
         sunSetHour: null,
         sunSetMinute : null,
         currentHourTime: null,
@@ -63,25 +63,30 @@ updateRemaining(hoursLeft, minutesleft, hours, minutes, currentHour, currentMinu
 
 calculateRemaining(){
     if (this.state.hasSunset === true) {
-    var secondsToHours = this.props.utc/3600; // if 18000, this is is -5.
-    var sunsetArr = this.state.sunsetTime.split(":"); // starts converting string to int
-    var sunSetHour = parseInt(sunsetArr[0]); //12
+    var secondsToHours = this.props.utc/3600; 
+    var sunsetArr = this.state.sunsetTime.split(":"); 
+    var sunSetHour = parseInt(sunsetArr[0]);
+    var pmIndicator = sunsetArr[2].split(" "); // getting to AM or PM
+    console.log(pmIndicator[1]);
+    if (pmIndicator[1] === "AM" && sunSetHour<12){
+        sunSetHour = sunSetHour + 12
+    }
     var sunSetMinute = parseInt(sunsetArr[1]);
-    var sunSetUserTimeHour = Math.abs(sunSetHour + secondsToHours); //creates sunset in user timezone // 7
+    var sunSetUserTimeHour = Math.abs(sunSetHour + secondsToHours); //creates sunset in user timezone 
     var sunSetUserTimeMinute = sunSetMinute; // creates sunset in user timezone   
 
-    var armyTimeHour = 24 - (12 - sunSetUserTimeHour); // converts hours to army time // this should be 19
+    var armyTimeHour = 24 - (12 - sunSetUserTimeHour); // converts hours to army time // 
  
 
-    var realHoursLeft = armyTimeHour - this.props.hour // with chicago, it should be 18000 utc //19 - 10 or 9
+    var realHoursLeft = armyTimeHour - this.props.hour 
     var currentHour = this.props.hour
+    console.log(realHoursLeft + " BEFORE ANY TIME DIFFERENCE")
 
-    if (this.props.utc != -14400) { //-14400 is EST
-        var timeDifference = Math.abs((this.props.utc - -14400) / 3600) // should be 1
-        realHoursLeft = realHoursLeft + timeDifference + 1 //9 - 1 or 8
-        console.log("creating a timeDifference")
+    if (this.props.utc !== -14400) { //-14400 is EST
+        var timeDifference = Math.abs((this.props.utc - -14400) / 3600) 
+        realHoursLeft = realHoursLeft + timeDifference + timeDifference 
+        console.log("creating a timeDifference and the realhours " + realHoursLeft)
         currentHour = currentHour - timeDifference;
-        sunSetUserTimeHour = sunSetUserTimeHour + timeDifference;
     }
 
     var minutesleft = sunSetUserTimeMinute - this.props.minute
@@ -95,7 +100,7 @@ calculateRemaining(){
         minutesleft = 60 + minutesleft
     } 
 
-    // adjust for daylight saving time
+    
     console.log("the sunset occurs at"+ (sunSetUserTimeHour)+ "and" + sunSetUserTimeMinute)
     console.log(sunSetUserTimeHour + "sunset in user time");
     console.log(armyTimeHour + "sunset in army time in user time");
@@ -109,18 +114,17 @@ calculateRemaining(){
 
 }
 
-//{this.fetchSunset()} TO NOT OVERUSE MY API CALLS
 
 render(){
     return(
         <div>
         <p> we are in the sunset - TO BE REMOVED </p>
             
-            
+            {this.fetchSunset()}
             {this.calculateRemaining()}
-        <p> current time in the location is {this.state.currentHourTime} : {this.state.currentMinuteTime} </p>
-        <p> sunset: {this.state.sunSetHour} : {this.state.sunSetMinute} pm </p> 
-        <p> sunset: {this.state.sunsetTime} - to be removed </p>
+        <p> current time in the zip code is {this.state.currentHourTime} : {this.state.currentMinuteTime}  </p>
+        <p> sunset at the zip code : {this.state.sunSetHour} : {this.state.sunSetMinute} pm </p> 
+
         <p> you have {this.state.remainingHours} hours and {this.state.remainingMinutes} minutes to sunset - get moving! </p>
        
         </div>
