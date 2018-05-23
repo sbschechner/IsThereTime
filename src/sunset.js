f4bf779ebad494361a22423f4f2f75abba52c3ca
benjy <benjamin.schechner@gmail.com>
2018-05-23 08:57:28 -0400

    printing sunset now

--- src/sunset.js
+++ src/sunset.js
@@ -8,8 +8,11 @@ class Sunset extends Component {
     this.state = {
         sunsetTime: null,
         hasSunset: false,
+        sunSetHour: null,
+        sunSetMinute : null,
         remainingHours: null,
         remainingMinutes: null,
+        count : 1,
         };
 
 this.getSunset = this.getSunset.bind(this);
@@ -40,26 +43,55 @@ fetchSunset(){
     }
 }
 
-calculateRemaining(){
-    var secondsToHours = this.props.utc/3600;
-    var sunsetArr = this.state.sunsetTime.split(":");
-    var sunSetHour = parseInt(sunsetArr[0],8);
-    var sunSetMinute = parseInt(sunsetArr[1],8);
-    var sunSetUserTimeHour = sunSetHour + secondsToHours;
-    var sunSetUserTimeMinute = sunSetMinute;
+updateRemaining(hoursLeft, minutesleft, hours, minutes){
 
-console.log(sunSetHour + "hour")
-console.log(sunSetMinute + " minute")
-console.log(sunSetUserTimeHour + "math hour")
-
-    if (this.state.hasSunset === true) {
+    if (this.state.count === 1 && this.state.hasSunset === true){
         this.setState({
-        remainingHours : sunSetUserTimeHour
+            remainingHours : hoursLeft,
+            remainingMinutes : minutesleft,
+            sunSetHour : hours,
+            sunSetMinute : minutes,
+            count: 2,
+            })
+
     }
-    
-    this.setState({
-        hasSunset : false
-    })
+}
+
+calculateRemaining(){
+    if (this.state.hasSunset === true) {
+    var secondsToHours = this.props.utc/3600;
+    var sunsetArr = this.state.sunsetTime.split(":"); // starts converting string to int
+    var sunSetHour = parseInt(sunsetArr[0]);
+    var sunSetMinute = parseInt(sunsetArr[1]);
+    var sunSetUserTimeHour = Math.abs(sunSetHour + secondsToHours); //creates sunset in user timezone
+    var sunSetUserTimeMinute = sunSetMinute; // creates sunset in user timezone   
+
+    var armyTimeHour = 24 - (12 - sunSetUserTimeHour); // converts hours to army time
+ 
+    var realHoursLeft = armyTimeHour - (sunSetUserTimeHour)
+
+    var minutesleft = sunSetUserTimeMinute - this.props.minute
+    if (minutesleft<0){  //prevents negative numbers 
+       realHoursLeft = realHoursLeft - 1
+        
+        if (realHoursLeft < 0){ // prevents negative numbers
+            realHoursLeft = 0
+        }
+
+        minutesleft = 60 + minutesleft
+    } 
+
+    // adjust for daylight saving time
+    console.log("the sunset occurs at"+ (sunSetUserTimeHour)+ "and" + sunSetUserTimeMinute)
+  //  console.log(timeDifference +" this is the time difference");
+    console.log(sunSetUserTimeHour + "sunset in user time");
+    console.log(armyTimeHour + "sunset in army time in user time");
+    console.log(realHoursLeft + "hours left should be subtracted from army time +1 for savings");
+   
+   this.updateRemaining(realHoursLeft, minutesleft, sunSetUserTimeHour, sunSetUserTimeMinute)
+
+    }
+
 }
 
 
@@ -71,6 +103,7 @@ render(){
             
             {this.fetchSunset()}
             {this.calculateRemaining()}
+        <p> sunset: {this.state.sunSetHour} : {this.state.sunSetMinute} </p> 
         <p> sunset: {this.state.sunsetTime} - to be removed </p>
         <p> you have {this.state.remainingHours} hours and {this.state.remainingMinutes} minutes to sunset - get moving! </p>
        
